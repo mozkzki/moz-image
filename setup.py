d2b418ffb6b974e4537eec1a70bf9955f84ba05e
requires.io <support@requires.io>
2021-06-21 01:55:59 +0900

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     python_requires=">=3.6",
-    install_requires=["requests==2.25.1", "python-dotenv==0.17.1", "pillow==8.2.0"],
+    install_requires=["requests==2.25.1", "python-dotenv==0.18.0", "pillow==8.2.0"],
     entry_points="""
         [console_scripts]
         {app} = {app}.cli:main
